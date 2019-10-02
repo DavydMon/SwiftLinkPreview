27f694265ba5644b6e247dfb2a582db06bce8c77
Kurnosov <akthesnipe@gmail.com>
2019-10-02 14:00:28 +0500

    simplified for ios

--- Sources/SwiftLinkPreview.swift
+++ Sources/SwiftLinkPreview.swift
@@ -398,6 +398,8 @@ extension SwiftLinkPreview {
         otherResponse = self.crawlDescription(otherResponse.htmlCode, result: otherResponse.result)
         
         otherResponse = self.crawlPrice(otherResponse.htmlCode, result: otherResponse.result)
+        
+        return otherResponse.result;
 
         return self.crawlImages(otherResponse.htmlCode, result: otherResponse.result)
     }
